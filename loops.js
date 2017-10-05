18205c679dd0ea47102c50b4d85d86ec458fca47
apocalypse575 <apocalypse575@gmail.com>
2017-10-05 00:01:46 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,31 @@
-for (let i = 0; i < 25; i++) {
-  if (i = 1) {
-    console.log(`I am 1 strange loop.`)
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if (i == 1) {
+    array.push(`I am 1 strange loop.`)
   }
   else {
-    return `I am ${i} strange loops.`
+    array.push(`I am ${i} strange loops.`)
   }
+}
+return array
+}
+
+function whileLoop(number) {
+  while (number != 0) {
+    console.log(number)
+    number--
+  }
+  return 'done'
+}
+
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do {
+  array.length--
+} while (array.length > 0 && maybeTrue());
+
+return array
 }
